88d8ff9081814d25e26d4ba48426899c4b9d6b1c
Maciej <dark.3at@gmail.com>
2023-08-30 00:22:36 +0200

    added menu files

---
+++ src/menu/menu_controll.py
@@ -0,0 +1,125 @@
+import os
+import time
+import numpy as np
+
+from enum import Enum
+from ctypes import *
+from src.scan_data import ContinuousMode
+from src.scan_data import ImCont, SampleMode, Status, ScanParam, DwfData, Logtime, PictureData, PictureSCS
+from src.files_operation import FileOperations
+from src.menu.menu_param import MenuParams
+
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
+CONST_MENU_SAMPLE_MODE_REPEAT = 13
+CONST_MENU_CONTINUOUS_MODE_REPEAT = 15
+
+width, height = os.get_terminal_size()
+
+class MenuControll:
+   
+    @staticmethod
+    def execute(user_input):
+        global MenuParams
+        data_list = user_input.split()
+        MenuControll_name = '%s%s'%("param_", data_list[0])
+        try:
+            controller = getattr(MenuParams, MenuControll_name)
+        except AttributeError:
+            DwfData.logError = "Method not found."
+        else:
+            elements = len(data_list) 
+            if (elements == 1):
+                controller('NoN')
+            elif(elements > 1):
+                controller(data_list[1])
+            else:
+                DwfData.logError = "Empty parameter."
+
+
+    @staticmethod
+    def menu_header():
+        txt = "XY Scanner - STM / AFM"
+        header = txt.center(width)
+        print("="*width)
+        print(header,)
+        print("="*width)
+    
+    
+    @staticmethod
+    def show_menu_parameters():
+        print ("")
+        print("Scan sample: \t", SampleMode.sample, "     ")
+        print("Resolution: \t", ScanParam.resolution, "     ")
+        print("Scan area: \t", (ScanParam.area), "     ")
+        print("Scan area: \t", (ScanParam.oxy), "     ")
+        print("Offset X: \t", (ScanParam.offset_x), "     ")
+        print("Offset Y: \t", (ScanParam.offset_y), "     ")
+        if(ScanParam.mode == Status.SAMPLE):
+            print("Scan freq: \t", SampleMode.hzAcq[0].value, "     ")
+        else:
+            print("Scan freq: \t", ContinuousMode.hzAcq[0].value, "     ")
+            print("Phase ch1: \t", ContinuousMode.phase_ch1, "     ")
+            print("Phase ch2: \t", ContinuousMode.phase_ch2, "     ")
+            
+        print("="*width)
+        print("DWF Ver: \t ", DwfData.version, "     ")
+        print("Log: \t\t", DwfData.logError, "     ")
+        print("-"*width)
+        print(" " * width, end='\r')
+
+
+    @staticmethod
+    def menu_param_revrite(revrite):
+        for i in range (revrite):
+                print(LINE_UP, end='\r')
+
+
+    @staticmethod
+    def menu_end():
+        print("-"*width)
+        txt = "Exit program - Come back soon! :-) "
+        goodby_txt = txt.center(width)
+        print(goodby_txt)
+        print("-"*width)
+
+
+    @staticmethod
+    def run():
+        MenuControll.show_menu_parameters()
+        user_input = 0
+        user_input = input("Get param: \t ")
+        if not (user_input == ''):
+            MenuControll.execute(user_input)
+        else:
+            DwfData.logError = "Empty prompt."
+        
+        if(DwfData.clear_menu == True):
+            DwfData.clear_menu = False
+            
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            
+            MenuControll.menu_header()
+            # MenuControll.show_menu_parameters()
+            
+            # if(ScanParam.mode != Status.SAMPLE):
+            #     MenuControll.menu_param_revrite(CONST_MENU_SAMPLE_MODE_REPEAT)
+            # else:
+            #     MenuControll.menu_param_revrite(CONST_MENU_CONTINUOUS_MODE_REPEAT)
+                
+        else:
+            if(ScanParam.mode == Status.SAMPLE):
+                MenuControll.menu_param_revrite(CONST_MENU_SAMPLE_MODE_REPEAT)
+            else:
+                MenuControll.menu_param_revrite(CONST_MENU_CONTINUOUS_MODE_REPEAT)
+            
+
+# test purpuse only
+# def main():
+#     while(ScanParam.scan != Status.EXIT):
+#         MenuControll.run()
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ src/menu/menu_param.py
@@ -0,0 +1,132 @@
+import os
+import time
+import numpy as np
+
+from enum import Enum
+from ctypes import *
+from src.scan_data import ImCont, SampleMode, ContinuousMode, Status, ScanParam, DwfData, Logtime, PictureData, PictureSCS
+from src.files_operation import FileOperations
+
+
+class MenuParams:
+
+## DATA:
+
+    @staticmethod
+    def param_sample(data_params):
+        """Nr of Samples [200-20000]"""
+        if(ScanParam.mode == Status.SAMPLE):
+            SampleMode.sample = int(data_params) % 20000
+            SampleMode.DataCH1 = (c_double*SampleMode.sample)()
+            SampleMode.DataCH2= (c_double*SampleMode.sample)()
+            SampleMode.f_ch1 = np.arange(SampleMode.sample, dtype=float)
+            SampleMode.f_ch2 = np.arange(SampleMode.sample, dtype=float)
+            
+        if(ScanParam.mode == Status.CONTINUOUS):
+            ContinuousMode.sample = int(data_params) % 20000
+            ContinuousMode.DataCH1 = (c_double*SampleMode.sample)()
+            ContinuousMode.DataCH2= (c_double*SampleMode.sample)()
+            ContinuousMode.f_ch1 = np.arange(SampleMode.sample, dtype=float)
+            ContinuousMode.f_ch2 = np.arange(SampleMode.sample, dtype=float)
+        
+    @staticmethod
+    def param_area(data_params):
+        """Scan Area [1-100] """
+        ScanParam.area = int(data_params)
+
+    @staticmethod
+    def param_res(data_params):
+        """Resolution [50-500] """
+        ScanParam.resolution = int(data_params)
+
+    @staticmethod
+    def param_freq(data_params):
+        """Scan Frequency """
+        dana = (int(data_params) % 2000) * 1000
+        SampleMode.hzAcq[0] = c_double(dana)
+
+    @staticmethod
+    def param_save(data_params):
+        """Save picture [yes, no]"""
+        PictureData.save = Status(data_params)
+    
+    @staticmethod
+    def param_stat(data_params):
+        """____"""
+        ScanParam.scan = Status(data_params)
+    
+    @staticmethod
+    def param_oxy(data_params):
+        """____"""
+        ScanParam.oxy = float(data_params)
+
+    @staticmethod
+    def param_dx(data_params):
+        """____"""
+        ImCont.x = float(data_params)
+    
+    @staticmethod
+    def param_dy(data_params):
+        """____"""
+        ImCont.y = float(data_params)
+    
+    @staticmethod
+    def param_phase1(data_params):
+        """DAC 1 - phase shift"""
+        if(ScanParam.mode == Status.CONTINUOUS):
+            ContinuousMode.phase_ch1 = float(data_params)
+        else:
+            DwfData.logError = "NO Continuous mode ON !"
+
+    @staticmethod
+    def param_phase2(data_params):
+        """DAC 2 - phase shift"""
+        if(ScanParam.mode == Status.CONTINUOUS):
+            ContinuousMode.phase_ch2 = float(data_params)
+        else:
+            DwfData.logError = "NO Continuous mode ON !"
+        
+## INSTRUCTIONS:
+
+    @staticmethod
+    def param_scan(data_params):
+        """Scan: Start"""
+        named_tuple = time.localtime()
+        time_info = time.strftime("%H-%M-%S", named_tuple)
+        DwfData.logTime.start = time_info
+        ScanParam.scan = Status.START
+
+    @staticmethod
+    def param_stop(data_params):
+        """Scan: Stop"""
+        ScanParam.scan = Status.STOP
+
+    @staticmethod
+    def param_save(data_params):
+        """File: Save"""
+        named_tuple = time.localtime()
+        time_info = time.strftime("%H-%M-%S", named_tuple)
+        FileOperations.save_manager_files(time_info)
+        # ScanParam.scan = Status.SAVE
+
+    @staticmethod
+    def param_exit(data_params):
+        """Exit program"""
+        ScanParam.scan = Status.EXIT
+    
+    @staticmethod
+    def param_inter(data_params):
+        """Interpolate picture [yes, no]"""
+        PictureSCS.interpolate = Status(data_params)
+
+    @staticmethod
+    def param_log(data_params):
+        """Show Logs from Analog Discovery 2 [yes, no]"""
+        DwfData.logStat = Status(data_params)
+    
+    @staticmethod
+    def param_mode(data_params):
+        """Scan Modes [sample, continuous]"""
+        ScanParam.mode = Status(data_params)
+        DwfData.clear_menu = True
+
